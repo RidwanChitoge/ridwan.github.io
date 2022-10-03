30d13dab86e943b444789dd1cd87f9835cb21934
RidwanChitoge <ridwanawaludin714@gmail.com>
2022-10-03 16:31:38 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2022 Ridwan Chitoge <a href="https://code-projects.org/">Code-chitoge</a></strong>
+    </div>
+</footer>
